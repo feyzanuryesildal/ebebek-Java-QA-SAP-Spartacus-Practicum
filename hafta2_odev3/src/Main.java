c52f058818369c6ce7c1e73c1b5178d46e8c1ef3
feyza <feyzanuryesildal@hotmail.com>
2022-11-05 00:50:38 +0300

    hafta 2 odev 3

---
+++ hafta2_odev3/src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int year = 2018;
+        boolean leap = false;
+
+        if(year % 4 == 0)
+        {
+            if( year % 100 == 0)
+            {
+                if ( year % 400 == 0)
+                    leap = true;
+                else
+                    leap = false;
+            }
+            else
+                leap = true;
+        }
+        else
+            leap = false;
+
+        if(leap)
+            System.out.println(year + " now years.");
+        else
+            System.out.println(year + " not now years.");
+    }
+}
