ed94718c0b715008091c3fdd9c7d5f4331344fe5
feyza <feyzanuryesildal@hotmail.com>
2022-11-19 22:16:51 +0300

    week 3 hom 6

---
+++ hafta3_odev6/src/Main.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+public class Main {
+    static Scanner getNumber=new Scanner(System.in);
+    public static void main(String[] args) {
+
+        int base,powerOf;
+        do {
+            System.out.print("Please enter base number: ");
+            base = getNumber.nextInt();
+            if(base<0) break;
+            System.out.print("Please enter the power: ");
+            powerOf = getNumber.nextInt();
+            System.out.println("Result is: "+GetExponential(base, powerOf));
+        }while(true);
+
+    }
+    public static double GetExponential(int b,int p){
+
+        if(p==0) return 1;
+        else return b*GetExponential(b,(p-1));
+    }
+}
